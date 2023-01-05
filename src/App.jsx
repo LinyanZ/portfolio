1cedef5d876dc17172ac665e13584d66db4a7c7c
LinyanZ <linyanz1@student.unimelb.edu.au>
2023-01-05 20:33:59 +1100

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,23 @@ function App() {
   });
 
   return (
-    <div className="canvas-container">
-      <Canvas>
-        <Perf position="top-left" />
-        <OrbitControls />
-        {particles && <Particles />}
-      </Canvas>
-    </div>
+    <>
+      <div className="landing-section p-10 mx-auto max-w-7xl mt-40 ">
+        <h1 className="text-6xl text-neutral-200 m-4 px-12 py-4 backdrop-blur-md bg-black/40 w-fit">
+          I'M LINYAN ZHU
+        </h1>
+        <h1 className="text-6xl text-neutral-200 m-4 px-12 py-4 backdrop-blur-md bg-black/40 w-fit">
+          WELCOME TO MY PORTFOLIO
+        </h1>
+      </div>
+      <div className="canvas-container">
+        <Canvas>
+          <Perf position="top-left" />
+          <OrbitControls />
+          {particles && <Particles />}
+        </Canvas>
+      </div>
+    </>
   );
 }
 
